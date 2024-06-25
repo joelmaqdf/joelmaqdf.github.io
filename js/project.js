f63cd7be11041b5d8c64d52bacd4eb374bef0bf8
joelmaqdf <joelmaqdf@gmail.com>
2024-06-25 22:31:42 +0100

    Backgroud updated

--- js/project.js
+++ js/project.js
@@ -2,16 +2,19 @@ const PROJECTS = [
     {
         id: 'cake_design',
         name: 'Cake Design Project',
-        description: ' CakeJF is a web project dedicated to the art of custom cakes. By blending Bootstrap’s elegance with the creativity of a cake designer, this site provides a delightful experience for visitors.This project was created to explore the bootstrap framework and practice code development and design.',
+        description: 'CakeJF is a web project dedicated to the art of custom cakes. By blending Bootstrap’s elegance with the creativity of a cake designer, this site provides a delightful experience for visitors.This project was created to explore the bootstrap framework and practice code development and design.',
         technologies: ['HTML', 'CSS', 'JAVASCRIPT', 'BOOTSTRAP'],
-        link: ''
+        link: '',
+        imgPafh:'img\projects\cake_design\cake_page.png',
     },
     {
         id: 'flower_design',
         name: 'Flower Design Project',
         description: 'This project was created to explore the bootstrap framework and practice code development and design.',
         technologies: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
-        link: ''
+        link: '',
+        imgPafh:'img\projects\flower\flower_page.png'
+        
     }
 ]
 
@@ -20,6 +23,7 @@ const projectId = urlParams.get('name');
 const descriptionRef = document.getElementById('description');
 const technologiesRef = document.getElementById('technologies');
 const linkRef = document.getElementById('link');
+const imgPafhRef = document.getElementById('projectImage'); 
 const technologyGradients= {
     'HTML': 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,52,1) 0%, rgba(180,5,5,1) 0%, rgba(204,28,8,1) 100%, rgba(234,88,48,1) 100%, rgba(252,116,62,1) 100%, rgba(252,176,69,1) 100%)',
     'CSS': 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(16,111,204,1) 0%, rgba(47,61,215,1) 0%, rgba(38,75,212,1) 0%, rgba(20,139,232,1) 100%, rgba(7,186,247,1) 100%, rgba(0,212,255,0.11257002801120453) 100%)',
@@ -33,6 +37,7 @@ function startUpPage() {
     console.log(selectedProject);
     descriptionRef.textContent = selectedProject.description;
     technologiesRef.innerHTML = '';
+    imgPafhRef.setAttribute('src', selectedProject.imgPafh);
 
     // Add the technology tags:
     selectedProject.technologies.forEach((technologia) => {
@@ -44,6 +49,7 @@ function startUpPage() {
     });
 
     linkRef.href = selectedProject.link;
+   
     linkRef.textContent = selectedProject.link ? 'Project Link' : 'No Link Available';
     
 }
